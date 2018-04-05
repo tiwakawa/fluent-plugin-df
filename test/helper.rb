4f16bea008d8ad89ccf7f65b89175a72da64f0a5
Kenji Okimoto <okimoto@clear-code.com>
2018-04-05 11:33:07 +0900

    Cleanup
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- test/helper.rb
+++ test/helper.rb
@@ -1,19 +1,8 @@
-require 'rubygems'
 require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
 require 'test/unit'
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
+$LOAD_PATH.unshift(__dir__)
 
 require 'fluent/test'
 require 'fluent/plugin/in_df'
-
-class Test::Unit::TestCase
-end
